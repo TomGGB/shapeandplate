bd180ea9637817c4a72cae076734f77f30adef97
TomGGB <lana.tomi.gabriel@gmail.com>
2024-08-26 15:38:16 -0400

    chore: Actualizar configuración DEBUG a False en settings.py

--- perfil/views.py
+++ perfil/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import Usuario  # Asegúrate de tener un modelo llamado Usuario
+from core.models import User  # Asegúrate de tener un modelo llamado Usuario
 from django.views.decorators.csrf import csrf_exempt  # Importa csrf_exempt
+from django.contrib.auth.decorators import login_required  # Importa login_required
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 from google.cloud import bigquery
 from dotenv import load_dotenv
 import os
@@ -27,53 +30,66 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = temp_file_path
 client = bigquery.Client()
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            auth_login(request, user)
+            print('Usuario autenticado')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Correo electrónico o contraseña incorrectos.')
     return render(request, 'login.html')
 
 def logout(request):
     return render(request, 'logout.html')
 
+@csrf_exempt
 def signup(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        diet = request.POST.get('diet', '')
+        weight = request.POST.get('weight', None)
+        height = request.POST.get('height', None)
+        smoker = request.POST.get('smoker', False) == 'on'
+        weekly_exercise_hours = request.POST.get('weekly_exercise_hours', None)
+
+        # Verifica si el correo electrónico ya está registrado
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'El correo electrónico ya está registrado.')
+            return render(request, 'signup.html')
+
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            diet=diet,
+            weight=weight,
+            height=height,
+            smoker=smoker,
+            weekly_exercise_hours=weekly_exercise_hours
+        )
+
+        user.save()
+
+        # Autenticar y loguear al usuario
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')  # Redirigir a la página de perfil después del registro
+
     return render(request, 'signup.html')
 
+@login_required
 def profile(request):
-    return render(request, 'profile.html')
-@csrf_exempt
-def save_data(request):
-    if request.method == 'POST':
-        nombre = request.POST['nombre']
-        apellidos = request.POST['apellidos']
-        edad = request.POST['edad']
-        altura = request.POST['altura']
-        ejercicio = request.POST['ejercicio']
-        dieta = request.POST['dieta']
-
-        # Construye un diccionario con los datos a insertar
-        data = {
-            #se crea un UID para cada usuario
-            "id": str(uuid.uuid4()),
-            "nombre": nombre,
-            "apellidos": apellidos,
-            "edad": edad,
-            "altura": altura,
-            "ejercicio": ejercicio,
-            "dieta": dieta
-        }
-
-        # Define el ID de la tabla (ajusta según tu proyecto y conjunto de datos)
-        table_id = "shapeandplate.workout.usuario"
-
-        # Inserta los datos en la tabla
-        errors = client.insert_rows_json(table_id, [data])
-
-        #Tambien guardar los datos en la base de datos de Django
-        usuario = Usuario(id=data["id"], nombre=data["nombre"], apellidos=data["apellidos"], edad=data["edad"], altura=data["altura"], ejercicio=data["ejercicio"], dieta=data["dieta"])
-        usuario.save()
-
-        if errors == []:
-            # Redirigir a una página de confirmación o de inicio
-            return redirect('workout')
-        else:
-            # Manejar errores, por ejemplo, mostrar un mensaje de error al usuario
-            return render(request, 'error.html', {'error_message': 'Error al guardar los datos'})
-
-    return render(request, 'workout.html')
+    if request.user.is_authenticated:
+        # El usuario está autenticado
+        return render(request, 'profile.html', {'user': request.user})
+    else:
+        # El usuario no está autenticado
+        return redirect('login')
