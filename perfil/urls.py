f941b7055bf92d65f12104824c8f51bb437e8559
TomGGB <lana.tomi.gabriel@gmail.com>
2024-08-26 14:09:07 -0400

    feat: Reorganizar rutas en el archivo urls.py y actualizar enlaces en base.html

--- perfil/urls.py
+++ perfil/urls.py
@@ -3,8 +3,8 @@ from . import views
 from workout import views as workout_views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('profile/', views.profile, name='profile'),
+    path('', views.profile, name='profile'),
+    path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('signup/save_data/', views.save_data, name='save_data'),
