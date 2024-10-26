e8563ef56bfd5805c71bb858735fc3af4e6f12a4
tomggb <lana.tomi.gabriel@gmail.com>
2024-10-25 21:37:09 -0300

    Refactor home URLs to remove unnecessary imports and simplify code

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home.views import index as index
+from home.views import index
 
 urlpatterns = [
     path('', index, name='home'),
--- shapeandplate/urls.py
+++ shapeandplate/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
-from workout.views import index  
-from home.views import index as index
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('workout/', include('workout.urls')),
-    path('', index, name='home'),
+    path('', include('home.urls')),
     path('', include('pwa.urls')),
     path('perfil/', include('perfil.urls')),
     path('plate/', include('plate.urls')),
